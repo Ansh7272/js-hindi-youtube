ab6d51bc0d7b8be6a606b3cf42c4b9b4b7cc2ccd
Ansh7272 <120785449+Ansh7272@users.noreply.github.com>
2024-08-05 18:03:11 +0000

    pre

---
+++ basics/05_datatype.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js scode as newer version 
+
+// alert(3+3)  we are using node js not browser
+
+/*
+number=> 2 to power 53
+bigint
+string
+boolean
+null
+undefined
+symbol => unique
+*/
+
+// object
+
+// typeof
